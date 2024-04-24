9bfaeb6eea3d36efed5a9d7d69281159a49d2425
kostek001 <kostek0020@gmail.com>
2024-04-24 23:26:09 +0200

    add slimevr package

--- pkgs/games/slimevr.nix
+++ pkgs/games/slimevr.nix
@@ -1,30 +1,33 @@
-{ appimageTools, fetchurl }:
+{ appimageTools, fetchurl, glib }:
 let
   name = "slimevr";
-  version = "0.12.0-rc.1";
-  hash = "sha256-+TVLgFVgmu4URZP4O+K6UQJAD3QG+TbPsIIVSfJBMAc=";
+  version = "0.12.0-rc.4";
 
-  src = fetchurl {
-    url = "https://github.com/SlimeVR/SlimeVR-Server/releases/download/v${version}/SlimeVR-amd64.appimage";
-    inherit hash;
-  };
+  src = appimageTools.extract {
+    inherit name version;
 
-  appimageContents = appimageTools.extract {
-    inherit name version src;
+    src = fetchurl {
+      url = "https://github.com/SlimeVR/SlimeVR-Server/releases/download/v${version}/SlimeVR-amd64.appimage";
+      hash = "sha256-olMoz1tAjtTTAgfM5qFbFsCTKn29ni0QeXh4sbsmFMs=";
+    };
+
+    # Update glib in appimage, otherwise java will crash
+    postExtract = ''
+      rm $out/usr/lib/libglib-2.0.so.0
+      ln -s ${glib}/lib/libglib-2.0.so.0 $out/usr/lib/libglib-2.0.so.0
+    '';
   };
 in
-appimageTools.wrapType2 {
+appimageTools.wrapAppImage {
   inherit name version src;
 
-  extraPkgs = pkgs: with pkgs; [ 
-    cargo-tauri
+  extraPkgs = pkgs: with pkgs; [
     jdk17
-    libusb1
   ];
 
   extraInstallCommands = ''
-    install -m 444 -D ${appimageContents}/slimevr.desktop $out/share/applications/slimevr.desktop
-    install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/256x256@2/apps/slimevr.png \
+    install -m 444 -D ${src}/slimevr.desktop $out/share/applications/slimevr.desktop
+    install -m 444 -D ${src}/usr/share/icons/hicolor/256x256@2/apps/slimevr.png \
       $out/share/icons/hicolor/256x256@2/apps/slimevr.png
     substituteInPlace $out/share/applications/slimevr.desktop \
       --replace 'Exec=slimevr' 'Exec=${name}'
