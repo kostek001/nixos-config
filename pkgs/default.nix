fb6cc7bd9cfdd9510796fc567966e632336f6fa9
kostek001 <kostek0020@gmail.com>
2024-06-04 13:56:05 +0200

    remove CAP_SYS_NICE patch

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,4 @@ final: prev:
 with final;
 
 {
-  scrumplex.kernelPatches.cap_sys_nice_begone = import misc/cap_sys_nice_begone;
 }
--- pkgs/misc/cap_sys_nice_begone/default.nix
+++
@@ -1,4 +0,0 @@
-{
-  name = "cap_sys_nice_begone";
-  patch = ./cap_sys_nice_begone.patch;
-}
