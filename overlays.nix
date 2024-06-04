2be40ad9dc273b13d730bbfcf161b010e4d9d231
kostek001 <kostek0020@gmail.com>
2024-06-04 14:02:47 +0200

    fix typos in overlays.nix

--- overlays.nix
+++ overlays.nix
@@ -1,16 +1,17 @@
-{ pkgs, inputs, ... }:
+{ inputs, ... }:
 
 {
   nixpkgs.overlays = [
     (import ./pkgs)
+    
     (final: prev: {
       master = inputs.nixpkgs-master.legacyPackages.x86_64-linux;
     })
 
-    (final: prev:
-      prev.arrpc.overrideAttrs (final: prev: {
-        patches = [ ../../pkgs/misc/arrpc/log_ids.patch ];
-      })
-    )
+    (final: prev: {
+      arrpc = prev.arrpc.overrideAttrs (oldAttrs: {
+        patches = [ pkgs/misc/arrpc/log_ids.patch ];
+      });
+    })
   ];
 }
