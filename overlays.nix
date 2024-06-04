1a6ef5c72371a67a86bea2fc8463cd86a31812f6
kostek001 <kostek0020@gmail.com>
2024-06-04 19:25:02 +0200

    add nixd overlay

--- overlays.nix
+++ overlays.nix
@@ -5,13 +5,18 @@
     (import ./pkgs)
     
     (final: prev: {
-      master = inputs.nixpkgs-master.legacyPackages.x86_64-linux;
+      master = inputs.nixpkgs-master.legacyPackages.${prev.system};
     })
 
     (final: prev: {
       arrpc = prev.arrpc.overrideAttrs (oldAttrs: {
         patches = [ pkgs/misc/arrpc/log_ids.patch ];
       });
+    })
+
+    # TEMPORARY
+    (final: prev: {
+      nixd = prev.master.nixd;
     })
   ];
 }
