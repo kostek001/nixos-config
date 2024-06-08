f297c0b63909d7d0a91697e7ed0381fb03210f3e
kostek001 <kostek0020@gmail.com>
2024-06-08 23:59:43 +0200

    remove gnome.nix

--- modules/default.nix
+++ modules/default.nix
@@ -5,12 +5,11 @@
     boot/plymouth.nix
     boot/loader/lanzaboote.nix
     desktop/hyprland
-    desktop/gnome.nix
     desktop/plasma.nix
     games/steam.nix
     hardware/adb.nix
     hardware/hid.nix
-    hardware/nvidia
+    hardware/nvidia.nix
     hardware/opentabletdriver.nix
     hardware/platformio.nix
     hardware/sound.nix
