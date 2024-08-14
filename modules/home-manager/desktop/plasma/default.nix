6032297fc84329d4a6ed7e25e7f918f7344dd75e
kostek001 <kostek0020@gmail.com>
2024-08-15 00:54:08 +0200

    update: plasma

--- modules/home-manager/desktop/plasma/default.nix
+++ modules/home-manager/desktop/plasma/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 let
   cfg = config.kostek001.desktop.plasma;
@@ -9,25 +9,173 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      (catppuccin-kde.override { flavour = [ "mocha" ]; accents = [ "blue" ]; })
+      (catppuccin.override { variant = "mocha"; accent = "blue"; themeList = [ "kvantum" ]; })
+      (afterglow-cursors-recolored.override { themeVariants = [ "Catppuccin" ]; catppuccinColorVariants = [ "Macchiato" ]; })
+      (catppuccin-papirus-folders.override { flavor = "mocha"; accent = "blue"; })
+
+      inputs.kostek001-pkgs.packages.${pkgs.system}.kde-material-you-colors
+      inputs.kostek001-pkgs.packages.${pkgs.system}.kde-material-you-colors.widget
+    ];
+
     programs.plasma = {
       enable = true;
+      overrideConfig = true;
+
+      workspace = {
+        clickItemTo = "select";
+
+        theme = "default";
+        colorScheme = "CatppuccinMochaBlue";
+        cursor.theme = "Afterglow-Recolored-Catppuccin-Macchiato";
+        iconTheme = "Papirus-Dark";
+        windowDecorations = {
+          library = "org.kde.breeze";
+          theme = "Breeze";
+        };
+        splashScreen.theme = "None";
+
+        wallpaper = builtins.toString ../../../../resources/background-2.jpeg;
+      };
+
+      panels = [
+        {
+          location = "top";
+          height = 40;
+          widgets = [
+            {
+              name = "org.kde.plasma.kickoff";
+              config = {
+                General = {
+                  icon = "pop-cosmic-applications";
+                  showActionButtonCaptions = false;
+                  alphaSort = true;
+                };
+              };
+            }
+            "org.kde.plasma.panelspacer"
+            {
+              name = "org.kde.plasma.icontasks";
+              config = {
+                General = {
+                  groupedTaskVisualization = 2;
+                  launchers = [
+                    "applications:firefox-beta.desktop"
+                    "preferred://filemanager"
+                    "applications:org.kde.konsole.desktop"
+                    "applications:steam.desktop"
+                    "applications:org.prismlauncher.PrismLauncher.desktop"
+                    "applications:codium-url-handler.desktop"
+                    "applications:vesktop.desktop"
+                  ];
+                };
+              };
+            }
+            "org.kde.plasma.panelspacer"
+            "org.kde.plasma.systemtray"
+            {
+              name = "org.kde.plasma.digitalclock";
+              config = {
+                Appearance = {
+                  showDate = false;
+                  # Font
+                  autoFontAndSize = false;
+                  fontFamily = "FreeMono";
+                  fontWeight = 700;
+                  boldText = true;
+                  fontSize = 36;
+                };
+              };
+            }
+          ];
+          floating = true;
+          screen = 0;
+        }
+      ];
+
+      kwin = {
+        titlebarButtons = {
+          left = [ ];
+          right = [ "help" "minimize" "maximize" "close" ];
+        };
+        effects = {
+          shakeCursor.enable = false;
+        };
+        virtualDesktops = {
+          rows = 1;
+          number = 4;
+        };
+
+        edgeBarrier = 50;
+        cornerBarrier = true;
+      };
 
       powerdevil.AC = {
+        powerButtonAction = "lockScreen";
         autoSuspend.action = "nothing";
-        turnOffDisplay.idleTimeoutWhenLocked = 20;
+        turnOffDisplay = {
+          idleTimeout = 300;
+          idleTimeoutWhenLocked = 20;
+        };
       };
 
       shortcuts = {
         "services/org.kde.konsole.desktop"."_launch" = "Meta+T";
+        kwin = {
+          "Switch to Desktop 1" = "Meta+F1";
+          "Switch to Desktop 2" = "Meta+F2";
+          "Switch to Desktop 3" = "Meta+F3";
+          "Switch to Desktop 4" = "Meta+F4";
+        };
+      };
+
+      kscreenlocker = {
+        autoLock = true;
+        lockOnResume = true;
+        passwordRequiredDelay = 5;
+        appearance = {
+          wallpaper = builtins.toString ../../../../resources/background-3.gif;
+        };
       };
 
       configFile = {
-        kscreenlockerrc = {
-          "Daemon"."Timeout" = 5;
-          "Greeter/Wallpaper/org.kde.image/General"."Image" = builtins.toString ../../../../resources/background-3.gif;
-          "Greeter/Wallpaper/org.kde.image/General"."PreviewImage" = builtins.toString ../../../../resources/background-3.gif;
+        kdeglobals."KFileDialog Settings" = {
+          "Allow Expansion" = false;
+          "Automatically select filename extension" = true;
+          "Breadcrumb Navigation" = true;
+          "Decoration position" = 2;
+          "LocationCombo Completionmode" = 5;
+          "PathCombo Completionmode" = 5;
+          "Show Bookmarks" = false;
+          "Show Full Path" = false;
+          "Show Inline Previews" = true;
+          "Show Preview" = false;
+          "Show Speedbar" = true;
+          "Show hidden files" = false;
+          "Sort by" = "Name";
+          "Sort directories first" = true;
+          "Sort hidden files last" = false;
+          "Sort reversed" = false;
+          "Speedbar Width" = 250;
+          "View Style" = "DetailTree";
         };
+
+        # Wallpaper fix
+        plasmarc.Wallpapers."usersWallpapers" = builtins.toString ../../../../resources/background-2.jpeg;
+
+        # Disable plasma-browser-integration notification
+        kded5rc.Module-browserintegrationreminder.autoload = false;
       };
+
+      # startup.desktopScript."panels_and_wallpaper".text = config.programs.plasma.startup.desktopScript."panels_and_wallpaper".text + ''
+      #   // Disable icons
+      #   let allDesktops = desktops();
+      #   for (const desktop of allDesktops) {
+      #     desktop.plugin = "org.kde.desktopcontainment";
+      #   }
+      # '';
     };
-  };
+  }
+  ;
 }
--- modules/nixos/desktop/plasma.nix
+++ modules/nixos/desktop/plasma.nix
@@ -18,14 +18,12 @@ in
     ];
 
     environment.systemPackages = with pkgs; [
-      kdePackages.qtstyleplugin-kvantum
+      # Previews for .webp, .tiff, .tga, .jp2 files
+      kdePackages.qtimageformats
 
-      # inputs.kostek001-pkgs.packages.${system}.plasma-smart-video-wallpaper-reborn
+      # inputs.kostek001-pkgs.packages.${system}.wallpaper-engine-kde-plugin
+      # qt6.qtwebsockets
       # qt6.qtmultimedia
-
-      inputs.kostek001-pkgs.packages.${system}.wallpaper-engine-kde-plugin
-      qt6.qtwebsockets
-      qt6.qtmultimedia
     ];
 
     programs.partition-manager.enable = true;
