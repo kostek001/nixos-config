af305875d0d4cb250bde4547d6f1600b1c40bf03
kostek001 <kostek0020@gmail.com>
2024-10-13 23:34:47 +0200

    fix minecraft <=1.20.1 crash

--- pkgs/overlays.nix
+++ pkgs/overlays.nix
@@ -12,17 +12,12 @@
       });
     })
 
-    # (final: prev: {
-    #   egl-wayland = prev.egl-wayland.overrideAttrs (oldAttrs: {
-    #     version = "c439cd596fb7eadae69012eaba013c39b2377771";
-    #     src = oldAttrs.src.override (_: {
-    #       hash = "sha256-MD+D/dRem3ONWGPoZ77j2UKcOCUuQ0nrahEQkNVEUnI=";
-    #     });
-    #   });
-    # })
-
     (final: prev: {
-      wlx-overlay-s = prev.master.wlx-overlay-s;
+      glfw3-minecraft = prev.glfw3-minecraft.overrideAttrs (oldAttrs: {
+        patches = oldAttrs.patches ++ [
+          ./misc/glfw3-minecraft/Dont-crash-on-calls-to-icon.patch
+        ];
+      });
     })
 
     # Temporary update to 0.0.6
