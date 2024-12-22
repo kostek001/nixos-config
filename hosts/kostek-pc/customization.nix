5a1566d62e02a6105d9c3f60b2e517d130a48f9f
kostek001 <kostek0020@gmail.com>
2024-12-22 12:44:20 +0100

    enable docker, pentesting module in kostek-pc config

--- hosts/kostek-pc/customization.nix
+++ hosts/kostek-pc/customization.nix
@@ -5,11 +5,17 @@
     (import ../../users/kostek/default.nix { username = "kostek"; fullname = "Kostek"; })
   ];
   knix.users.kostek.autologin.enable = true;
+  knix.users.kostek.pentesting.enable = true;
 
   knix.misc.virtualisation.enable = true;
   knix.users.kostek.vr.enable = true;
 
   home-manager.users.kostek = { ... }: {
     khome.games.vr.enable = true;
+  };
+
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
   };
 }
