578540a2d3ed386750f089811c56ebcbbd3077f4
kostek001 <kostek0020@gmail.com>
2025-01-17 23:00:35 +0100

    fix keybinding in users/kostek/home gnome module

--- users/kostek/home/modules/gnome.nix
+++ users/kostek/home/modules/gnome.nix
@@ -11,6 +11,9 @@ in
 
   config = mkIf cfg.enable {
     dconf.settings = {
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
+      };
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
         binding = "<Super>t";
         command = "kgx";
