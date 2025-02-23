93cfeb5908a6429463d61291e3fbba32b2d2ff2f
kostek001 <kostek0020@gmail.com>
2025-02-24 00:08:43 +0100

    enable docker, pelican-wings in sdell-1

--- hosts/sdell-1/customization.nix
+++ hosts/sdell-1/customization.nix
@@ -7,4 +7,37 @@
   users.users.root.hashedPasswordFile = config.age.secrets.userHashedPassword.path;
   users.users.serveradmin.hashedPasswordFile = config.age.secrets.userHashedPassword.path;
   users.users.serveradmin.openssh.authorizedKeys.keys = (import ../identities.nix).masterKeys;
+
+  ## CONTAINERS
+  virtualisation.docker.enable = true;
+  virtualisation.docker.autoPrune = {
+    enable = true;
+    dates = "daily";
+    flags = [ "--all" "--filter 'until=24h'" ];
+  };
+  knix.privileged.groups = [ "docker" ];
+  virtualisation.oci-containers.backend = "docker";
+
+  environment.persistence."/nix/persist".directories = [
+    "/var/lib/docker/"
+    "/etc/pelican/"
+    "/var/lib/pelican/"
+  ];
+  virtualisation.oci-containers.containers.pelican_wings = {
+    image = "ghcr.io/pelican-dev/wings:latest";
+    ports = [ "8519:8519" "8520:8520" ];
+    environment = {
+      WINGS_UID = "1000";
+      WINGS_GID = "1000";
+      WINGS_USERNAME = "wings";
+    };
+    volumes = [
+      "/var/run/docker.sock:/var/run/docker.sock"
+      "/var/lib/docker/containers/:/var/lib/docker/containers/"
+      "/etc/pelican/:/etc/pelican/"
+      "/var/lib/pelican/:/var/lib/pelican/"
+      "/var/log/pelican/:/var/log/pelican/"
+      "/tmp/pelican/:/tmp/pelican/"
+    ];
+  };
 }
