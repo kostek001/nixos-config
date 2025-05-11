3690a2f3a87304c9ab6dcab92e3a6834c7f92ade
kostek001 <kostek0020@gmail.com>
2025-05-11 13:44:44 +0200

    fix suspend with nvidia gpu

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 with lib;
 
 let
@@ -44,6 +44,42 @@ in
       # Temporary fix for https://github.com/NVIDIA/open-gpu-kernel-modules/issues/538
       "NVreg_EnableGpuFirmware=0"
     ];
+
+    systemd.services.systemd-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = mkIf config.hardware.nvidia.powerManagement.enable "false";
+
+    systemd.services."gnome-suspend" = mkIf config.hardware.nvidia.powerManagement.enable {
+      description = "suspend gnome shell";
+      before = [
+        "systemd-suspend.service"
+        "systemd-hibernate.service"
+        "nvidia-suspend.service"
+        "nvidia-hibernate.service"
+      ];
+      wantedBy = [
+        "systemd-suspend.service"
+        "systemd-hibernate.service"
+      ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = ''${pkgs.procps}/bin/pkill -f -STOP ${pkgs.gnome-shell}/bin/gnome-shell'';
+      };
+    };
+    systemd.services."gnome-resume" = mkIf config.hardware.nvidia.powerManagement.enable {
+      description = "resume gnome shell";
+      after = [
+        "systemd-suspend.service"
+        "systemd-hibernate.service"
+        "nvidia-resume.service"
+      ];
+      wantedBy = [
+        "systemd-suspend.service"
+        "systemd-hibernate.service"
+      ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = ''${pkgs.procps}/bin/pkill -f -CONT ${pkgs.gnome-shell}/bin/gnome-shell'';
+      };
+    };
 
     environment.variables = {
       # Required to run the correct GBM backend for nvidia GPUs on wayland
