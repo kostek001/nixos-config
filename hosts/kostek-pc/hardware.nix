0d5c40285987d6a6a7c18797e5009175355776dd
kostek001 <kostek0020@gmail.com>
2025-09-17 11:45:39 +0200

    update nvidia package to latest version in hosts/kostek-pc

--- hosts/kostek-pc/hardware.nix
+++ hosts/kostek-pc/hardware.nix
@@ -1,14 +1,26 @@
-{ ... }:
+{ config, ... }:
 
 {
+  ## BOOT
   knix.boot.loader.lanzaboote.enable = true;
 
   boot.kernelParams =
     # Hide "SGX disabled by BIOS."
     [ "nosgx" ];
 
+  ## NVIDIA
   knix.hardware.nvidia.enable = true;
+  # Use newer nvidia driver
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "580.82.09";
+    sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
+    sha256_aarch64 = "sha256-6tHiAci9iDTKqKrDIjObeFdtrlEwjxOHJpHfX4GMEGQ=";
+    openSha256 = "sha256-YB+mQD+oEDIIDa+e8KX1/qOlQvZMNKFrI5z3CoVKUjs=";
+    settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
+    persistencedSha256 = "sha256-lbYSa97aZ+k0CISoSxOMLyyMX//Zg2Raym6BC4COipU=";
+  };
 
+  ## DISKS
   # Enable TRIM
   boot.initrd.luks.devices."luks-fbdcf386-6e66-4d69-87cb-9511d65f7fbe".allowDiscards = true;
   boot.initrd.luks.devices."luks-318904c2-e121-4f98-9ea3-591e009f9f63".allowDiscards = true;
