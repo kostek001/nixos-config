061a575a5817941438c4863f26cd8770176ed61e
kostek001 <kostek0020@gmail.com>
2025-09-21 01:20:23 +0200

    update flake.lock, remove wivrn highPriority workaround in users/kostek/home/modules/games/vr.nix

--- users/kostek/nixos/modules/vr.nix
+++ users/kostek/nixos/modules/vr.nix
@@ -16,19 +16,8 @@ in
       openFirewall = false;
       defaultRuntime = true;
       autoStart = false;
+      highPriority = true;
       extraServerFlags = [ "--no-manage-active-runtime" "--no-publish-service" ];
-    };
-
-    # Fix REALTIME priority
-    security.wrappers."wivrn-server" = {
-      setuid = false;
-      owner = "root";
-      group = "root";
-      capabilities = "cap_sys_nice+eip";
-      source = getExe config.services.wivrn.package;
-    };
-    systemd.user.services.wivrn = {
-      serviceConfig = mkForce { ExecStart = builtins.concatStringsSep " " ([ "${config.security.wrapperDir}/wivrn-server" "--systemd" ] ++ config.services.wivrn.extraServerFlags); };
     };
   };
 }
